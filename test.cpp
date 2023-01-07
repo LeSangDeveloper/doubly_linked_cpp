7f89138553caa94036637572a07dffcaea7645d4
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-01-07 17:08:07 +0800

    finish project

--- test.cpp
+++ test.cpp
@@ -1,5 +1,12 @@
 #include "doubly_linked_list.h"
+#include<iostream>
+
+using namespace std;
 
 int main() {
+    DLinkedList<double> a;
+    a.addFront(2.5);
+    a.addBack(1.2);
+    cout << a.front() << " " << a.back() << endl;
     return 1;
 }
